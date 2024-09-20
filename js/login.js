b61ccb95152da588e2e690370014c46201246dca
antasin <antasin95@gmail.com>
2024-09-20 17:12:08 +0700

    added add money  event handler

--- js/login.js
+++ js/login.js
@@ -9,12 +9,27 @@
 document
   .getElementById("button-login")
   .addEventListener("click", function (event) {
-    //step-2 prevent default behaviour(prevent reloading browser)
+    //step-2 prevent default behaviour(prevent reloading browser).reload korbe default
     event.preventDefault(); //<----- vajal for biggeners
     console.log("Login button clicked");
     //step -3 get the phone number
-    const PhoneNumber=document.getElementById('phone-number').value;
-    //previous line without .view id te ja likhi show korbe na   
-    console.log(PhoneNumber);
-
+    const PhoneNumber = document.getElementById("phone-number").value;
+    const pinNumber = document.getElementById("pin-number").value;
+    //previous line without .view id te ja likhi show korbe na
+    console.log(PhoneNumber, pinNumber);
+    //step-4 validate phone and pin
+    //this is not the ideal way its just temporary.you shouldnot do like this
+    if (PhoneNumber === "5" && pinNumber === "1234") {
+      console.log("you are logged in");
+      window.location.href = '/home.html'
+      //step-5  allow user to use the website
+    } else {
+      alert("wrong number or pin");
+    }
+  });
+document
+  .getElementById("button-login")
+  .addEventListener("click", function (event) {
+    console.log("Button Click");
+  event.preventDefault();
   });
